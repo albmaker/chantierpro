172b8d4249c644c9ff2046596c190e02e45b1b20
albmaker <alban.butiin43@gmail.com>
2026-07-05 20:38:23 +0200

    Corrige navigation devis guide

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
+
 import BottomNav from './components/BottomNav'
+
 import Dashboard from './pages/Dashboard'
 import DevisList from './pages/DevisList'
 import DevisDetail from './pages/DevisDetail'
@@ -20,6 +22,7 @@ import ExportData from './pages/ExportData'
 import WizardDevis from './pages/WizardDevis'
 import VoiceInput from './pages/VoiceInput'
 import { BlogList, BlogPost } from './pages/Blog'
+
 import { DataProvider, useData } from './contexts/DataContext'
 import { ThemeProvider } from './contexts/ThemeContext'
 
@@ -34,7 +37,16 @@ function LoadingScreen() {
 function AppContent() {
   const location = useLocation()
   const { loading } = useData()
-  const hideNavRoutes = ['/', '/auth', '/pricing']
+
+  // On masque la navigation du bas sur les pages qui ont déjà leurs propres boutons fixes.
+  const hideNavRoutes = [
+    '/',
+    '/auth',
+    '/pricing',
+    '/wizard-devis',
+    '/voice-input',
+  ]
+
   const blogRoutes = location.pathname.startsWith('/blog')
   const showNav = !hideNavRoutes.includes(location.pathname) && !blogRoutes
 
@@ -58,6 +70,7 @@ function AppContent() {
         <Route path="/factures" element={<FacturesList />} />
         <Route path="/clients" element={<Clients />} />
         <Route path="/stats" element={<Stats />} />
+
         <Route path="/parametres" element={<Parametres />} />
         <Route path="/profil" element={<Parametres />} />
 
@@ -72,6 +85,7 @@ function AppContent() {
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+
       {showNav && <BottomNav />}
     </div>
   )
